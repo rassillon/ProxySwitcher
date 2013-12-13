a33d35cc28d2298bd38f1a0f00c1b95e655d863f
rassillon <rassillondev@gmail.com>
2013-12-13 12:49:20 +0100

    Beta v0.1 Release
    - Rearranged code braces
    - All methods working correctly
    - Proxy retrieve and save working well

--- ProxySwitcher/Form1.cs
+++ ProxySwitcher/Form1.cs
@@ -5,39 +5,38 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProxySwitcher
-{
-    public partial class Form1 : Form
-    {
+namespace ProxySwitcher {
+    public partial class Form1 : Form {
         ProxyManager pm;
 
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
             pm = new ProxyManager();
             updatePanel();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        /// <summary>
+        /// When program loads, calls this method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e) {
             textBox1.Text = pm.getProxyURL();
             textBox2.Text = pm.getProxyPort();
             label4.Text = "Program loaded successfully.";
             label4.ForeColor = Color.Green;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (pm.getProxyURL() == "")
-            {
+        // ENABLE Button Click
+        private void button1_Click(object sender, EventArgs e) {
+            if (pm.getProxyURL() == "") {
                 label4.Text = "ERROR: Input Proxy!";
                 label4.ForeColor = Color.Red;
-            }
-            else
-            {
+            } else {
                 pm.setProxyState(true);
                 label4.Text = "Proxy Enabled succesfully.";
                 label4.ForeColor = Color.Green;
@@ -45,46 +44,38 @@ namespace ProxySwitcher
             updatePanel();
         }
 
-
-        private void button2_Click(object sender, EventArgs e)
-        {
+        // DISABLE Button Click
+        private void button2_Click(object sender, EventArgs e) {
             pm.setProxyState(false);
             label4.Text = "Proxy Disabled successfully.";
             label4.ForeColor = Color.Green;
             updatePanel();
         }
 
-        private void updatePanel()
-        {
-            if (pm.getProxyState())
-            {
+        /// <summary>
+        /// This method is used to update the description label in panel
+        /// </summary>
+        private void updatePanel() {
+            if (pm.getProxyState()) {
                 panel2.BackColor = Color.Green;
                 label1.Text = "ENABLED";
-                
-            }
-            else
-            {
+            } else {
                 panel2.BackColor = Color.Red;
                 label1.Text = "DISABLED";
             }
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
+    
+        // Apply Button Click
+        private void button3_Click(object sender, EventArgs e) {
             string t1 = textBox1.Text, t2 = textBox2.Text;
-            if (t1.Contains("") )
-            {
+            if (t1.Contains(":") ) {
                 label4.Text = "ERROR: Input Proxy!";
                 label4.ForeColor = Color.Red;
-            }
-            else
-            {
+            } else {
                 pm.setProxy(textBox1.Text, textBox2.Text);
                 label4.Text = "Proxy set successfully";
                 label4.ForeColor = Color.Green;
             }
-            
         }
-
     }
 }
--- ProxySwitcher/Program.cs
+++ ProxySwitcher/Program.cs
@@ -5,35 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProxySwitcher
-{
-    static class Program
-    {
+namespace ProxySwitcher {
+    static class Program {
         public static string ApplicationName = "ProxySwitcher";
 
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            
         }
     }
 
-    public class ProxyManager
-    {
+    public class ProxyManager {
 
         private string URL, port;
 
-        public ProxyManager()
-        {
-
+        public ProxyManager() {
+            URL = getProxyURL();
+            port = getProxyPort();
         }
 
-        public string getProxyURL()
-        {
+        public string getProxyURL() {
             string res = "";
             res = (String)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
                 "ProxyServer", "");
@@ -44,53 +37,39 @@ namespace ProxySwitcher
             return res;
         }
 
-        public string getProxyPort()
-        {
+        public string getProxyPort() {
             string res = "";
             res = (String)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
                 "ProxyServer", "");
-            if (res.Length > 0)
-            {
+            if (res.Length > 0) {
                 URL = res.Substring(res.IndexOf(":") + 1);
                 res = URL;
             }
             return res;
-
         }
 
-        public void setProxy(string proxy)
-        {
-            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-                "ProxyServer", proxy);
-        }
-
-        public void setProxy(string proxy, string port)
-        {
+        public void setProxy(string proxy, string port) {
             Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
                 "ProxyServer", proxy + ":" + port);
         }
 
-        public bool getProxyState()
-        {
+        public bool getProxyState() {
             int res;
             bool resb = false;
             res = (int)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
                 "ProxyEnable", 0);
 
-            if (res == 1)
-            {
+            if (res == 1) {
                 resb = true;
             }
             return resb;
         }
                 
-        public void setProxyState(bool state)
-        {
+        public void setProxyState(bool state) {
             int res = 0;
             if (state) res=1;
             Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
                 "ProxyEnable", res);
         }
-
     }
 }
